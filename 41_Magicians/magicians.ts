65dc707cdad08e6686af8cf256e55e5fd1073620
DELL <afridi2542@gmail.com>
2024-04-03 23:17:55 +0500

    first commit

---
+++ 41_Magicians/magicians.ts
@@ -0,0 +1,20 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), 
+// which prints the name of each magician in the array.
+
+let magician: string[] = ['Harry potter', 'Hermione granger', 'Charlie chapler', 'Albus'];
+
+function make_great (magicianArray:String[]){
+    
+
+}
+
+function show_magicians(magicians: string[]){
+
+    magicians.forEach(element => {
+        console.log(element);
+        
+        
+    });
+
+}
+show_magicians(magician)
